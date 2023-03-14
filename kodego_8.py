ca1262b1562cd41bfeaa2dca68dda540a765309a
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-14 14:19:31 +0800

    Answered question number 8.

---
+++ kodego_8.py
@@ -0,0 +1,9 @@
+print("Alan thinks of a number. He squares it, then takes away 5, next multiplies it by 4, takes away 7, divides it by 3 and finally adds 6. His answer is 9. What number did Alan start with?")
+
+possible_numbers = 6, 3, 2, 4
+
+formula = (((((3 ** 2)-5)*4)-7)/3)+6
+
+print(formula)
+
+print("The answer is 3")
