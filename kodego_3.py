46562abf8367297d38d1991d8e1007af5d1cda46
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-14 13:12:52 +0800

    Answered the third question

---
+++ kodego_3.py
@@ -0,0 +1,12 @@
+print("If the hour hand of a clock is turned anticlockwise from 2 pm to 9 am, through how many degrees will it have turned?")
+
+full_rotation = 360
+
+deg_per_hour = 360/12
+hours_inbetween = 5
+degrees_inbetwen = (360/12)*5
+
+print(f"A full rotation is 360 degrees and there are 12 hours in a clock-face.")
+print(f"We'd get {deg_per_hour} degrees for every hour if we divide the degrees per hour: '360/12'.")
+print("There are 5 hours if we manually count anti-clockwise from 2pm to 9am.")
+print(f"The degrees from an anti-clockwise 2pm to 9pm movement is {degrees_inbetwen} degrees: (360/12)*5.")
