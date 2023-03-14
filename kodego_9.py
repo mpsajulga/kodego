503f3a6e702b070125b7d577226d0ba85864e6f0
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-14 14:12:33 +0800

    Answered question 9.

---
+++ kodego_9.py
@@ -0,0 +1,2 @@
+print("You are facing North. Turn 90 degrees left. Turn 180 degrees right. Reverse direction. Turn 45 degrees left. Reverse direction. Turn 270 degrees right. In which direction are you now facing?")
+
