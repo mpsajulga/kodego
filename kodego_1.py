934b930e68fc0578b3458192dafd7d87c2f9612b
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-14 12:16:52 +0800

    Uploaded initial files

---
+++ kodego_1.py
@@ -0,0 +1,5 @@
+print("Grace thought of a number, added 7, multiplied by 3, took away 5 and divided by 4 to give an answer of 7. What was her starting number?")
+
+formula = (((4 + 7) * 3) - 5) / 4
+print(f"The formula to answer this question is 'formula = (((4 + 7) * 3) - 5) / 4'")
+print(formula)
